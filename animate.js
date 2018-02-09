e3e84289c33f4e911cac979390c1b6fc078f3017
Karen Chen <kchen6600@gmail.com>
2018-02-08 19:50:56 -0500

    updated

--- animate.js
+++ animate.js
@@ -1,7 +1,32 @@
 var canv = document.getElementById("slate");
 var ctx = canv.getContext("2d");
+
 var requestID;
 var stopB = document.getElementById("stop");
+var circleB = document.getElementById("circling");
+var bounceB = document.getElementById("bouncing");
+
+var radius = 20;
+var x = canv.width/2;
+var y = canv.height/2;
+
+var xcor = x;
+var ycor = y;
+var angle = Math.random()*Math.PI*2;
+var xfact;
+var yfact;
+if( Math.cos(angle) > 0 ){ 
+	xfact = 1; 
+}
+else{ 
+	xfact = -1; 
+}
+if( Math.sin(angle) > 0 ){ 
+	yfact = 1; 
+}
+else{ 
+	yfact = -1; 
+}
 
 var animate = function(){
 	var grow = true;
@@ -36,6 +61,34 @@ var animate = function(){
 	draw();
 };
 
+var animate2 = function(){
+    var draw2 = function(){
+	
+	ctx.clearRect(0,0,canv.width,canv.height);
+	
+	ctx.beginPath();
+	ctx.arc(xcor,ycor,20,0,2*Math.PI);
+	ctx.stokeStyle = "#800000";
+	ctx.stroke();
+ 	ctx.fillStyle = "#800000";
+	ctx.fill();
+	
+	if( xcor-radius < 0 || xcor+radius > canv.width ){
+	    xfact *=-1;
+	}
+	else if(ycor-radius < 0 || ycor+radius > canv.height){
+	    yfact *=-1;
+	}
+
+	xcor+=Math.cos(angle)*xfact;
+	ycor+=Math.sin(angle)*yfact;
+	
+	requestID = window.requestAnimationFrame(draw2);
+	console.log(requestID);
+    }
+    draw2();
+}
+
 
 var clear = function() {
     ctx.clearRect(0, 0, 500, 500);
@@ -45,6 +98,6 @@ var pause = function(){
 	window.cancelAnimationFrame(requestID);
 }
 
-stopB.addEventListener('click', pause)
-
-canv.addEventListener('click', animate);
+stopB.addEventListener('click', pause);
+circleB.addEventListener('click', animate);
+bounceB.addEventListener('click', animate2);
